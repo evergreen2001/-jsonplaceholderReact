4c506adef239f971a9c14c52833251cb523aba84
evergreen2001 <dicksonevergreenido@gmail.com>
2020-11-01 01:10:42 +0100

    adding firebase

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
 import React from "react";
-import Home from "../src/component/Home";
-import UserDetails from "./component/UserDetails";
+import Title from "../src/component/Title";
+
 import "./App.css";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
-    <Router>
-      <div className="App container">
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/userdetails" component={UserDetails} />
-        </Switch>
-      </div>
-    </Router>
+    <div className="App container">
+      <Title />
+    </div>
   );
 }
 
---
+++ src/component/Title.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Title = (params) => {
+
+    return(
+
+<div className="container">
+
+
+    <h1>FireGal</h1>
+
+    <p>Your color</p>
+</div>
+    
+    )
+    
+}
