ab243329d88082f2512319c5f1d8af1b479477cc
evergreen2001 <dicksonevergreenido@gmail.com>
2020-11-03 03:46:23 +0100

    :firebase error fixed

--- src/hooks/useStorage.js
+++ src/hooks/useStorage.js
@@ -3,15 +3,13 @@ import { useState, useEffect } from "react";
 import { storage } from "../firebase/config";
 
 const useStorage = (file) => {
-  const [progess, setProgess] = useState(0);
+  const [progress, setProgress] = useState(0);
 
   const [error, setError] = useState(null);
 
   const [url, setUrl] = useState(null);
 
   useEffect(() => {
- 
-
     const storageRef = storage.ref(file.name);
 
     storageRef.put(file).on(
@@ -19,19 +17,19 @@ const useStorage = (file) => {
       (snap) => {
         let percent = (snap.bytesTransferres / snap.totalBytes) * 100;
 
-        setProgess(percent);
+        setProgress(percent);
       },
       (err) => {
         setError(err);
       }
-    ),
+    ,
       async () => {
         const url = await storageRef.getDownloadURl();
         setUrl(url);
-      };
+      });
   }, [file]);
 
-  // return { progress, url, error };
+  return { progress, url, error };
 };
 
 export default useStorage;
