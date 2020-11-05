7e97864a543fdf5a12e59b41ae80b157deef5c7b
evergreen2001 <dicksonevergreenido@gmail.com>
2020-11-05 13:18:44 +0100

    adding custom hook to usefirestore

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import Title from "../src/component/Title";
 
 import "./App.css";
 import UploadForm from "./component/UploadForm";
+import ImageGrid from "./component/ImageGrid";
 
 function App() {
   return (
     <div className="App container">
       <Title />
 <UploadForm/>
+
+<ImageGrid/>
     </div>
   );
 }
---
+++ src/hooks/useFirestore.js
@@ -0,0 +1,23 @@
+
+
+import {useState , useEffect} from 'react';
+
+import { firestore} from '../firebase/config'
+
+const useFirestore = (params) => {
+
+    const [docs, setDocs] = useState([])
+
+
+
+    return(
+
+
+docs
+
+    )
+    
+}
+
+
+export default useFirestore
